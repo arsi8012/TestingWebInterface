44ee366817307c51f158fdca0652a331b4c96ae4
Oleg Atlasov <olegkui@mail.ru>
2021-10-26 16:39:46 +0400

    Invalid input 2

--- src/test/java/ru/netology/TestWebInterface.java
+++ src/test/java/ru/netology/TestWebInterface.java
@@ -1,15 +1,11 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestWebInterface {
 
