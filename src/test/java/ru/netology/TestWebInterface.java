5885a24c6b3172a784770aab8bfb9d882203188f
Oleg Atlasov <olegkui@mail.ru>
2021-10-26 16:36:13 +0400

    Invalid input

--- src/test/java/ru/netology/TestWebInterface.java
+++ src/test/java/ru/netology/TestWebInterface.java
@@ -23,4 +23,36 @@ public class TestWebInterface {
         form.$("button").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldInputNameOnLatinLetters() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Aliev Geidar");
+        form.$("[data-test-id=phone] input").setValue("+79472512638");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldInputWrongNumber() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Алиев Гейдар");
+        form.$("[data-test-id=phone] input").setValue("+7947251263");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldWithoutInstallCheckbox() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Алиев Гейдар");
+        form.$("[data-test-id=phone] input").setValue("+79472512638");
+        form.$("button").click();
+        $("[class='checkbox__text']").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
 }
