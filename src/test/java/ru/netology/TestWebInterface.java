a01183d1ae1af7ade06e45e2499c9ad8d1981312
Oleg Atlasov <olegkui@mail.ru>
2021-10-26 15:55:33 +0400

    Success submission

---
+++ src/test/java/ru/netology/TestWebInterface.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWebInterface {
+
+    @Test
+    void shouldSuccessSubmissionOfForm() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[class='form form_size_m form_theme_alfa-on-white']");
+        form.$("[data-test-id=name] input").setValue("Алиев Гейдар");
+        form.$("[data-test-id=phone] input").setValue("+79472512638");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
